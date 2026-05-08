d0de3738854b9ec3f530b72c32050f74302cfd4e
Engjell Voca <vo.en0312@gmail.com>
2026-05-08 15:27:04 +0200

    inital project skeleton

---
+++ application/orchestrator.py
@@ -0,0 +1,3 @@
+# builds the application by orchestrating the different components together
+# uses only common layer and core constracts without depending on specific implementations so we can 
+# easily swap out different models and algorithms
